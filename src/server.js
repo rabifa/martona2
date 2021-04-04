73a2067fa3f88d935cf7826f6d220ac35cd1833c
Raphael Faria <rabifa@hotmail.com>
2021-04-03 22:30:27 -0300

    public folder was set in the server. Now the scripts, images and styles works

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const server = express()
 
+// anable statics files
+server.use(express.static("public"))
+
 // request, response
 server.get('/', (req, res) => {
   return res.sendFile(__dirname + "/views/index.html")
