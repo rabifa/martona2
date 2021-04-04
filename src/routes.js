eab6a3b02148b9392f2806afe5eacad3eff67229
Raphael Faria <rabifa@hotmail.com>
2021-04-03 23:11:14 -0300

    fix path

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ const basePath = __dirname + "/views"
 // request, response
 routes.get('/', (req, res) => res.sendFile(basePath + "/index.html"))
 routes.get('/job', (req, res) => res.sendFile(basePath + "/job.html"))
-routes.get('/job-edit', (req, res) => res.sendFile(basePath + "/job-edit.html"))
+routes.get('/job/edit', (req, res) => res.sendFile(basePath + "/job-edit.html"))
 routes.get('/profile', (req, res) => res.sendFile(basePath + "/profile.html"))
 
 
