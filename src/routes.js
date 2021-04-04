63e181cd44092bae83ed7ced058851a40690603a
Raphael Faria <rabifa@hotmail.com>
2021-04-03 23:42:57 -0300

    update path in the routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const routes = express.Router()
 
-const basePath = __dirname + "/views"
+// EJS by default understands that the files are inside the views folder, 
+// but in the root of the app not inside of another folder
+const views = __dirname + "/views/"
 
 // request, response
-routes.get('/', (req, res) => res.sendFile(basePath + "/index.html"))
-routes.get('/job', (req, res) => res.sendFile(basePath + "/job.html"))
-routes.get('/job/edit', (req, res) => res.sendFile(basePath + "/job-edit.html"))
-routes.get('/profile', (req, res) => res.sendFile(basePath + "/profile.html"))
+routes.get('/', (req, res) => res.render(views + "index"))
+routes.get('/job', (req, res) => res.render(views + "job"))
+routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
+routes.get('/profile', (req, res) => res.render(views + "profile"))
 
 
 module.exports = routes
