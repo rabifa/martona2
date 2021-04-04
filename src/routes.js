58b312f988cd71b567fae17a4f223fd86a8320ed
Raphael Faria <rabifa@hotmail.com>
2021-04-04 00:22:36 -0300

    a new profile info was created in the routes.js file and the variables off EJS were implemented in the profile.ejs file

--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,20 @@ const routes = express.Router()
 // but in the root of the app not inside of another folder
 const views = __dirname + "/views/"
 
+const profile = {
+  name: "Raphael",
+  avatar: "https://avatars.githubusercontent.com/u/30870174?s=400&u=a9266803e7f730a651e86d42d9dd8b20ddbd3826&v=4",
+  "monthly-budget": 3000,
+  "hours-per-day": 5,
+  "days-per-week": 25,
+  "vacation-per-year": 8,
+}
+
 // request, response
 routes.get('/', (req, res) => res.render(views + "index"))
 routes.get('/job', (req, res) => res.render(views + "job"))
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
-routes.get('/profile', (req, res) => res.render(views + "profile"))
+routes.get('/profile', (req, res) => res.render(views + "profile", {profile}))
 
 
 module.exports = routes
