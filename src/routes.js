ccd6592a7555232ea6615497319a473b56e86f95
Raphael Faria <rabifa@hotmail.com>
2021-04-04 22:02:11 -0300

    The jobs info was moved to routes.js file. I started to turn the jobs view list into a template.

--- src/routes.js
+++ src/routes.js
@@ -11,17 +11,39 @@ const profile = {
   "monthly-budget": 3000,
   "hours-per-day": 5,
   "days-per-week": 25,
-  "vacation-per-year": 8,
+  "vacation-per-year": 8
 }
 
-const jobs = []
+const jobs = [
+  {
+    id: 1,
+    name: "Pizzaria Guloso",
+    "daily-hours": 2,
+    "total-hours": 60,
+    created_at: Date.now()
+  },
+  {
+    id: 2,
+    name: "OneTwo Project",
+    "daily-hours": 3,
+    "total-hours": 47,
+    created_at: Date.now()
+  }]
 
 // request, response
-routes.get('/', (req, res) => res.render(views + "index"))
+routes.get('/', (req, res) => res.render(views + "index", {jobs}))
 routes.get('/job', (req, res) => res.render(views + "job"))
 routes.post('/job', (req, res) => {
-  // req.body receive the form data
-  jobs.push(req.body)
+  // req.body receive the form data -> { name: '', 'daily-hours': '', 'total-hours': ''}
+  const lastId = jobs[jobs.length - 1]?.id || 1
+
+  jobs.push({
+    id: lastId + 1,
+    name: req.body.name,
+    "daily-hours": req.body["daily-hours"],
+    "total-hours": req.body["total-hours"],
+    created_at: Date.now()
+  })
   return res.redirect('/')
 })
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
