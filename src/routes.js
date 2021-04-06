4eaa55c64910686b4c46a1800c4763aab2919b7d
Raphael Faria <rabifa@hotmail.com>
2021-04-05 21:57:47 -0300

    The delete job function was implemented

--- src/routes.js
+++ src/routes.js
@@ -93,7 +93,7 @@ const Job = {
 
     save(req, res) {
       // req.body receive the form data -> { name: '', 'daily-hours': '', 'total-hours': ''}
-      const lastId = Job.data[Job.data.length - 1]?.id || 1
+      const lastId = Job.data[Job.data.length - 1]?.id || 0
 
       Job.data.push({
         id: lastId + 1,
@@ -145,6 +145,15 @@ const Job = {
       })
 
       return res.redirect('/job/' + jobId)
+    },
+
+    delete(req, res) {
+      const jobId = req.params.id
+
+      Job.data = Job.data.filter(job => Number(job.id) !== Number(jobId))
+
+
+      return res.redirect('/')
     }
 
   },
@@ -178,6 +187,7 @@ routes.get('/job', Job.controllers.create)
 routes.post('/job', Job.controllers.save)
 routes.get('/job/:id', Job.controllers.show)
 routes.post('/job/:id', Job.controllers.update)
+routes.post('/job/delete/:id', Job.controllers.delete)
 routes.get('/profile', Profile.controllers.index)
 routes.post('/profile', Profile.controllers.update)
 
