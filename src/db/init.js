ac3428a262e061d00c9ea2fa33b3d5a60f4b72b4
Raphael Faria <rabifa@hotmail.com>
2021-04-17 15:53:37 -0300

    async/await implemented in init.js

--- src/db/init.js
+++ src/db/init.js
@@ -1,67 +1,73 @@
 const Database = require('./config')
 
-Database()
+const initDb = {
+  async init() {
 
-Database.exec(`CREATE TABLE profile (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT,
-  avatar TEXT,
-  monthly_budget INT,
-  days_per_week INT,
-  hours_per_day INT,
-  vacation_per_year INT,
-  value_hour INT
-)`)
+    const db = await Database()
 
-Database.exec(`CREATE TABLE jobs (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT,
-  daily_hours INT,
-  total_hours INT,
-  created_at DATETIME
-)`)
+    await db.exec(`CREATE TABLE profile (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      avatar TEXT,
+      monthly_budget INT,
+      days_per_week INT,
+      hours_per_day INT,
+      vacation_per_year INT,
+      value_hour INT
+    )`)
 
-Database.run(`INSERT INTO profile (
-  name,
-  avatar,
-  monthly_budget,
-  days_per_week,
-  hours_per_day,
-  vacation_per_year,
-  value_hour
-) VALUES (
-    "Raphael",
-    "https://github.com/rabifa.png",
-    3000,
-    5,
-    5,
-    8,
-    75  
-)`)
+    await db.exec(`CREATE TABLE jobs (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      daily_hours INT,
+      total_hours INT,
+      created_at DATETIME
+    )`)
 
-Database.run(`INSERT INTO jobs (
-  name,
-  daily_hours,
-  total_hours,
-  created_at,
-) VALUES (
-    "Pizzaria Guloso",
-    2,
-    1,
-    1618684053906
-)`)
+    await db.run(`INSERT INTO profile (
+      name,
+      avatar,
+      monthly_budget,
+      days_per_week,
+      hours_per_day,
+      vacation_per_year,
+      value_hour
+    ) VALUES (
+        "Raphael",
+        "https://github.com/rabifa.png",
+        3000,
+        5,
+        5,
+        8,
+        75  
+    )`)
 
-Database.run(`INSERT INTO jobs (
-  name,
-  daily_hours,
-  total_hours,
-  created_at,
-) VALUES (
-    "OneTwo Project",
-    3,
-    47,
-    1618684053906
-)`)
+    await db.run(`INSERT INTO jobs (
+      name,
+      daily_hours,
+      total_hours,
+      created_at,
+    ) VALUES (
+        "Pizzaria Guloso",
+        2,
+        1,
+        1618684053906
+    )`)
 
+    await db.run(`INSERT INTO jobs (
+      name,
+      daily_hours,
+      total_hours,
+      created_at,
+    ) VALUES (
+        "OneTwo Project",
+        3,
+        47,
+        1618684053906
+    )`)
 
-Database.close()
+    await db.close()
+  }
+}
+
+initDb.init()
