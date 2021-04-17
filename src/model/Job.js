935a333372ef602c323c8ddbb5f59962e538e4b5
Raphael Faria <rabifa@hotmail.com>
2021-04-17 17:34:40 -0300

    select data from jobs  db and input async/await in DashboardControllers

--- src/model/Job.js
+++ src/model/Job.js
@@ -1,23 +1,20 @@
-let data = [
-  {
-    id: 1,
-    name: "Pizzaria Guloso",
-    "daily-hours": 2,
-    "total-hours": 1,
-    created_at: Date.now(),
-  },
-  {
-    id: 2,
-    name: "OneTwo Project",
-    "daily-hours": 3,
-    "total-hours": 47,
-    created_at: Date.now(),
-  }
-]
+const Database = require('../db/config')
 
 module.exports = {
-  get(){
-    return data
+  async get(){
+    const db = await Database()
+
+    const data = await db.all(` SELECT * FROM jobs `)
+
+    await db.close()
+
+    return data.map(job => ({
+        id: job.id,
+        name: job.name,
+        "daily-hours": job.daily_hours,
+        "total-hours": job.total_hours,
+        created_at: job.created_at
+    }))
   },
   update(newJob){
     data = newJob
