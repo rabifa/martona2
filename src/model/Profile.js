be1bad5dd8803e4ab01a4672718fe93eadfed2ce
Raphael Faria <rabifa@hotmail.com>
2021-04-17 16:42:42 -0300

    select data from profile db and input async/await in DashboardContrllers

--- src/model/Profile.js
+++ src/model/Profile.js
@@ -1,16 +1,32 @@
-let data = {
-  name: "Raphael",
-  avatar: "https://github.com/rabifa.png",
-  "monthly-budget": 3000,
-  "hours-per-day": 5,
-  "days-per-week": 5,
-  "vacation-per-year": 8,
-  "value-hour": 75
-}
+const Database = require('../db/config')
+
+// let data = {
+//   name: "Raphael",
+//   avatar: "https://github.com/rabifa.png",
+//   "monthly-budget": 3000,
+//   "hours-per-day": 5,
+//   "days-per-week": 5,
+//   "vacation-per-year": 8,
+//   "value-hour": 75
+// }
 
 module.exports = {
-  get() {
-    return data
+  async get() {
+    const db = await Database()
+
+    const data = await db.get(` SELECT * FROM profile `)
+
+    await db.close()
+
+    return {
+      name:  data.name,
+      avatar: data.avatar,
+      "monthly-budget": data.monthly_budget,
+      "hours-per-day": data.hours_per_day,
+      "days-per-week": data.days_per_week,
+      "vacation-per-year": data.vacation_per_year,
+      "value-hour": data.value_hour
+    }
   },
 
   update(newData) {
